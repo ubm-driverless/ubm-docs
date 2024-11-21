276dd5f5cb8e16ae489296f89050d8cf158f2eee
FedericoCalzoni <federicocalzoni01@gmail.com>
2024-11-21 20:12:40 +0100

    reverting path names

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -19,7 +19,7 @@ for path in sorted(src.rglob("*.py")):
     
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path("PYTHON_PACKAGES", doc_path)
+    full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
 
@@ -39,6 +39,6 @@ for path in sorted(src.rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open("PYTHON_PACKAGES/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
     logging.info("Generated SUMMARY.md")
