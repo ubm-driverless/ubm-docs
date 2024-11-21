826523cfb4ad3ed4d773abaa940ef7b54fc45029
FedericoCalzoni <federicocalzoni01@gmail.com>
2024-11-21 18:22:43 +0100

    code inside 'old_stuff' folder gives problems. Skip it for now

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -10,6 +10,11 @@ root = Path(__file__).parent.parent
 src = root / "src"
 
 for path in sorted(src.rglob("*.py")):
+    
+    # Skip files containing 'deprecated' or 'old_stuff' in their path
+    if any(part in {"deprecated", "old_stuff"} for part in path.parts):
+        continue
+    
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
