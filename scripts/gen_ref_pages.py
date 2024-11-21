bd02afec0e28e2fbff6b1237198696bca9ed2a72
FedericoCalzoni <federicocalzoni01@gmail.com>
2024-11-21 18:47:19 +0100

    skipping launch files

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -15,7 +15,7 @@ for path in sorted(src.rglob("*.py")):
     logging.info(f"Processing file: {path}")
     
     # Skip files containing the following kywords in their path
-    if any(part in {"deprecated", "old_stuff", "f1tenth_system", "f1tenth_stack"} for part in path.parts):
+    if any(part in {"deprecated", "old_stuff", "f1tenth_system", "f1tenth_stack", "launch"} for part in path.parts):
         logging.info(f"Skipping: {path}")
         continue
     
